efa7cab6244985dee9c128683b9e89851a78af98
SilkWebDev <silk-web-dev@proton.me>
2023-10-20 13:33:38 +0700

    Grunt task for compiling styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.registerTask('default', ['sass', 'uglify', 'htmlmin', 'cssmin']);
+  grunt.registerTask('styles', ['sass', 'cssmin']);
   grunt.registerTask('all', [
     'sass',
     'uglify',
